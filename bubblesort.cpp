d9fef7ef5ce7c57f6bef93d73cc0239f0420e541
tripples <sanketsudake@gmail.com>
2013-03-30 00:40:16 +0530

    PPATH 2

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -7,10 +7,10 @@ int main(int argc, char *argv[])
     int element[100],N;
     cin >> N;
     for (int i = 0; i < N; i++)
-	cin >> element[i];
+        cin >> element[i];
     bubblesort(element, N);
     for (int i = 0; i < N; i++)
-	cout << element[i] << endl;
+        cout << element[i] << endl;
     return 0;
 }
 
@@ -18,11 +18,11 @@ void bubblesort(int element[],int N)
 {
     int temp;
     for(int i = 1; i < N; i++)
-	for(int j = 0; j < N-1; j++)
-	    if(element[j] > element[j + 1])
-		{
-		    temp = element[j];
-		    element[j] = element[j + 1];
-		    element[j + 1] = temp;
-		}
+        for(int j = 0; j < N-1; j++)
+            if(element[j] > element[j + 1])
+                {
+                    temp = element[j];
+                    element[j] = element[j + 1];
+                    element[j + 1] = temp;
+                }
 }
--- ppath.cpp
+++ ppath.cpp
@@ -1,19 +1,117 @@
 using namespace std;
 #include <iostream>
 #include <algorithm>
-int prime[1000];
-void pinit(int prime[], int n){
-  for(int i=0; i< 1000; i++)
-    prime[i] = i;
+#include <cassert>
+#include <queue>
+#include <set>
+
+bool arr[10000];
+int dist[10000];
+int visited[10000];
+void primegen(bool arr[], int n);
+inline int isprime(int no);
+int bitset(int curnode,int pos, int digit);
+int ppath(int source, int destination);
+int main(int argc, char *argv[])
+{
+    primegen(arr,10000);
+    int testcase;
+    testcase = 1;
+    cin >> testcase;
+    for(int i=0; i < testcase; i++)
+        {
+            int source, destination;
+            cin >> source >> destination;
+            int value = ppath(source, destination) ;
+            if(value == -1)
+                cout << "Impossible" << endl;
+            else
+                cout << value << endl;
+        }
+
+    return 0;
 }
-void pprint(int prime[], int n){
-  for(int i=0; i<1000; i++)
-    {
-      cout << prime[i] << "\t";
-    }
+/* Genearate prime nos upto 10000 */
+void primegen(bool arr[], int n)
+{
+    fill_n(arr, n, 1);
+    arr[0] = 0;
+    arr[1] = 0;
+    for (int i=2; i < n; i++)
+        {
+            if ((i*i) > n)
+                break;
+            if (arr[i])
+                {
+                    int j = 2;
+                    while (i*j < n)
+                        {
+                            arr[i*j] = 0;
+                            j = j + 1;
+                        }
+                }
+        }
 }
-int main(){
-  pinit(p, 1000);
-  pprint(p, 1000);
-  return 0;
+/* no prime or not
+   function not much of use
+*/
+inline int isprime(int no)
+{
+    if(arr[no])
+        return true;
+    return false;
+}
+/*  set particular bit to some digit*/
+int bitset(int curnode,int pos, int digit)
+{
+    int no = 1;
+    int temp = curnode;
+    int bit;
+    for(int i = 4 - pos ; i > 0; i--)
+        {
+            bit = temp % 10;
+            temp = temp / 10;
+            no = 10 * no;
+        }
+    no = no /10;
+    int newnode = curnode - (bit*no);
+    newnode = newnode + (digit * no);
+    return newnode;
+}
+/*
+  Taken some ideas form
+  http://stackoverflow.com/questions/3221134/help-with-algorithm-problem-from-spoj
+*/
+int ppath(int source, int destination)
+{
+    fill_n(dist, 10000, 0);
+    fill_n(visited, 10000, 1);
+    queue<int> q;
+    q.push(source);
+    visited[source] = 0;
+    dist[source] = 0;
+    while(!q.empty())
+        {
+            int curnode;
+            curnode = q.front();
+            int d = dist[curnode];
+            q.pop();
+            if(curnode == destination)
+                return dist[curnode];
+            for (int i = 0; i < 4; i++)
+                {
+                    for(int j = 0; j < 10; j++)
+                        {
+                            if (j%2 == 0 && i == 3) continue;
+                            int newnode = bitset(curnode, i, j);
+                            if(arr[newnode] && visited[newnode])
+                                {
+                                    q.push(newnode);
+                                    visited[newnode] = 0;
+                                    dist[newnode] = d + 1;
+                                }
+                        }
+                }
+        }
+    return -1;
 }
